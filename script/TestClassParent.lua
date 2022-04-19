47de449c925befd517d552d65745579da29945b1
Nicolas Lang <git@nicolas-lang.de>
2022-04-19 22:52:56 +0200

    updated example

--- script/TestClassParent.lua
+++ script/TestClassParent.lua
@@ -2,11 +2,11 @@
 ---@class TestClassParent
 local TestClassParent = {
     log = function(_, msg)
-        local msg = (((debug.getinfo(2, "n").name) or "") .. ":" .. (msg or ""))
+        local msg_text = (((debug.getinfo(2, "n").name) or "") .. ":" .. (msg or ""))
         if game and game.print then
-            game.print(msg)
+            game.print(msg_text)
         end
-        log(msg)
+        log(msg_text)
     end,
     new = function(_)
     end
